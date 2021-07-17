63b9d3b09e98ca5a07cfd5c18e2db6e57b4b7be0
KadVipPro <72777349+KadVipPro@users.noreply.github.com>
2021-07-17 21:01:58 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASG.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASG
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<DatabaseContext>();
+
+            services.AddMvc();
+            services.AddDbContext<DatabaseContext>(c => c.UseSqlServer
+                (
+                    Configuration.GetConnectionString("LongND")
+                )
+            );
+            services.AddDistributedMemoryCache();
+            services.AddSession(c =>
+            {
+                c.IOTimeout = TimeSpan.FromSeconds(20);
+                c.Cookie.HttpOnly = true;
+                c.Cookie.IsEssential = true;
+            }
+            );
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseHttpsRedirection();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Account}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
